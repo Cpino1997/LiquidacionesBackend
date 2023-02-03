520b5531191998efafba86411a81a28915565c6c
Cristian Pino <pino440@gmail.com>
2023-02-03 12:09:52 -0300

    ⚡️Test WorkFLow ⚡️️

--- src/test/java/cl/pinolabs/springreact/SpringReactApplicationTests.java
+++ src/test/java/cl/pinolabs/springreact/SpringReactApplicationTests.java
@@ -3,6 +3,8 @@ package cl.pinolabs.springreact;
 import cl.pinolabs.springreact.trabajadores.percistencia.crud.TestCrud;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
@@ -11,18 +13,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringReactApplicationTests {
+    private static final Logger logger = LoggerFactory.getLogger(SpringReactApplicationTests.class);
     @Autowired
     TestCrud testCrud;
 
     @Test
     @Order(1)
     void testCrearTest() {
+        logger.info("Creando la primera prueba!");
         cl.pinolabs.springreact.trabajadores.percistencia.entity.Test test = new cl.pinolabs.springreact.trabajadores.percistencia.entity.Test();
         test.setId(1);
         test.setMensaje("----- Funcionando -----");
         cl.pinolabs.springreact.trabajadores.percistencia.entity.Test result = testCrud.save(test);
+        logger.info("Se ha creado el usuario test!");
         assertNotNull(result);
-
         Optional<cl.pinolabs.springreact.trabajadores.percistencia.entity.Test> t = testCrud.findById(test.getId());
         assertTrue(t.isPresent());
     }
@@ -30,7 +34,9 @@ class SpringReactApplicationTests {
     @Test
     @Order(2)
     void testEliminarTest() {
+        logger.info("Creando la Segunda prueba!");
         testCrud.deleteById(1);
+        logger.info("Se ha eliminado el usuario test!");
         Optional<cl.pinolabs.springreact.trabajadores.percistencia.entity.Test> t = testCrud.findById(1);
         assertFalse(t.isPresent());
     }
