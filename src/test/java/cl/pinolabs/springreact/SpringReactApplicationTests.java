1a8672f70797a7b70eee9e5e5678949081134e86
Cristian Pino <pino440@gmail.com>
2023-02-03 11:50:45 -0300

    ⚡️Test WorkFLow ⚡️️

--- src/test/java/cl/pinolabs/springreact/SpringReactApplicationTests.java
+++ src/test/java/cl/pinolabs/springreact/SpringReactApplicationTests.java
@@ -5,8 +5,9 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringReactApplicationTests {
@@ -15,34 +16,23 @@ class SpringReactApplicationTests {
 
     @Test
     @Order(1)
-    void create() {
-        System.out.println("----- Cargando el Test 1 -----");
+    void testCrearTest() {
         cl.pinolabs.springreact.trabajadores.percistencia.entity.Test test = new cl.pinolabs.springreact.trabajadores.percistencia.entity.Test();
-        test.setId(2);
+        test.setId(1);
         test.setMensaje("----- Funcionando -----");
-        testCrud.save(test);
-        System.out.println("----- Se ha creado un usuario -----");
+        cl.pinolabs.springreact.trabajadores.percistencia.entity.Test result = testCrud.save(test);
+        assertNotNull(result);
+
         Optional<cl.pinolabs.springreact.trabajadores.percistencia.entity.Test> t = testCrud.findById(test.getId());
-        if(t.isPresent()){
-            System.out.println("----- Funcionando! -----");
-        }else{
-            System.out.println("----- No Funcionando =( -----");
-        }
+        assertTrue(t.isPresent());
     }
 
     @Test
     @Order(2)
-    void delete(){
-        System.out.println("----- Cargando el Test 2 -----");
-        Integer id = 2;
-        testCrud.deleteById(id);
-        Optional<cl.pinolabs.springreact.trabajadores.percistencia.entity.Test> t = testCrud.findById(id);
-        if (t.isPresent()){
-            System.out.println("----- No Se ha podido eliminar -----");
-        }else{
-            System.out.println("----- Eliminado con exito! -----");
-        }
+    void testEliminarTest() {
+        testCrud.deleteById(1);
+        Optional<cl.pinolabs.springreact.trabajadores.percistencia.entity.Test> t = testCrud.findById(1);
+        assertFalse(t.isPresent());
     }
-
-
 }
+
